8b795c103960798f7783363a642783f2efe1783e
Adam Brinckman <abrinckm@gmail.com>
2016-11-11 10:31:27 -0500

    Fix dynamic relationships for file and folder models

--- app/models/file.js
+++ app/models/file.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   parentType: DS.attr('string'), // folder or item
-  parentId: (this.get('parentType') === "collection")? DS.belongsTo('collection') : DS.belongsTo('folder'),
+  parentId: Ember.computed('parentType', function() {
+      return (this.get('parentType') === "collection")? DS.belongsTo('collection') : DS.belongsTo('folder');
+  }),
   name: DS.attr('string'),
   size: DS.attr('number'),
   mimeType: DS.attr('string'),
--- app/models/folder.js
+++ app/models/folder.js
@@ -1,10 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   _accessLevel: DS.attr(),
   _id: DS.attr(),
+  // _isParentFolder: DS.attr('boolean'),
+  // _isParentCollection: DS.attr('boolean'),
   baseParentType: DS.attr('string'), // folder or item
-  baseParentId: (this.get('baseParentType') === "collection")? DS.belongsTo('collection') : DS.belongsTo('folder'),
+  baseParentId: Ember.computed('baseParentType', function() {
+      return (this.get('baseParentType') === "collection") ? DS.belongsTo('collection') : DS.belongsTo('folder');
+  }),
   name : DS.attr('string'),
   description: DS.attr('string'),
   created: DS.attr('date'),
@@ -12,8 +17,9 @@ export default DS.Model.extend({
   public: DS.attr('boolean'),
   meta : DS.attr(), // this contains a creator object that contains a list of creators..
   parentCollection: DS.attr('string'), // folder or item
-  parentId: (this.get('parentCollection') === "collection")? DS.belongsTo('collection') : DS.belongsTo('folder'),
+  parentId: Ember.computed('parentCollection', function() {
+      return (this.get('parentCollection') === "collection")? DS.belongsTo('collection') : DS.belongsTo('folder');
+  }),
   size: DS.attr('number'),
   updated: DS.attr('date'),
 });
-
