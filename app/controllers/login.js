3bdde215be5852fb7c4a9ef150f3f294d55f5dd0
ian-taylor <ian.j.taylor@gmail.com>
2017-02-22 12:43:24 +0000

    abracadabra login button ! a missing model on the iterator ...

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,14 +3,14 @@ import EmberUploader from 'ember-uploader';
 
 export default Ember.Controller.extend({
   init() {
-    // var token = Ember.$.getJSON('https://girder.wholetale.org/api/v1/token/current');
-    // alert("Token is " + JSON.stringify(token));
+    //var token = Ember.$.getJSON('https://girder.wholetale.org/api/v1/token/current');
+    //alert("Token is " + JSON.stringify(token));
   },
   actions: {
     login: function(url) {
-      //var content = Ember.$.ajax(url);
+      var content = Ember.$.ajax(url);
       window.open(url, "_self");
-      //console.log(content);
+      console.log(content);
     },
   }
 });
