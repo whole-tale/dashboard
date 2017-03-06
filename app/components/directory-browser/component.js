9c56768f312ce44ba1d4ccc0786414e4d5354d55
Adam Brinckman <abrinckm@gmail.com>
2017-03-06 16:59:51 -0500

    working on a context menu for right click actions in directory browser

--- app/components/directory-browser/component.js
+++ app/components/directory-browser/component.js
@@ -5,6 +5,25 @@ import layout from './template';
 export default Ember.Component.extend({
     layout,
     showEditor : false,
+    didRender() {
+        let contextMenuElements = Ember.$('.contextmenu');
+        for(let i = 0; i < contextMenuElements.length; i++) {
+            let element = contextMenuElements[i];
+            element.oncontextmenu = function(mouseEvent) {
+                let ctxt = Ember.$('#directory-context-menu');
+                ctxt.css({
+                    top: mouseEvent.layerY+"px",
+                    left: mouseEvent.layerX+"px",
+                    position: "absolute"
+                });
+                ctxt.removeClass("hidden");
+                let selected = mouseEvent.path.find(offset => {
+                    return /contextmenu/.test(offset.classList.value);
+                });
+                return false;
+            };
+        };
+    },
     actions: {
       clickedFolder : function(item) {
         this.sendAction('action', item,  "true");
@@ -12,5 +31,9 @@ export default Ember.Component.extend({
       clickedFile: function(item) {
         this.sendAction('action', item, "false");
       },
+      hide: function() {
+          let ctxt = Ember.$('#directory-context-menu');
+          ctxt.addClass('hidden');
+      }
     }
 });
