f4a511a5cd7d3eb7d54b41a696bb06f15ed57c6e
Adam Brinckman <abrinckm@gmail.com>
2017-03-09 12:51:37 -0500

    Override default ember deleteRecord behavior. Ember expects a '204 no content' response when deleting a resource. Girder returns 200

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,41 +4,48 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  tokenHandler: Ember.inject.service("token-handler"),
+    tokenHandler: Ember.inject.service("token-handler"),
 
-  // Some Girder APIs need query params in the url to update models...
-  // This functions allows us to pass query params to save() like this:
-  // model.save({adapterOptions:{queryParams:{}}});
-  // where queryParams is a hash object.
-  urlForUpdateRecord(id, modelName, snapshot) {
-    let url = this._super(id, modelName, snapshot);
-    if(snapshot.adapterOptions.queryParams) {
-        let queryParams = snapshot.adapterOptions.queryParams;
-        let keys = Object.keys(queryParams);
-        let q = keys.reduce((_q, key) => {
-            _q.push(key+"="+queryParams[key]);
-            return _q;
-        }, []);
-        return url + "?"+q.join('&');
-    }
-    return url;
-  },
+    host: config.apiHost,
+    namespace: config.apiPath,
+    primaryKey: '_id',
 
-  methodForRequest(params) {
-//      console.log("MethodForRequest being called...");
-  //    console.log(params);
-      if (params.requestType === 'createRecord') { return 'PUT'; }
-      return this._super(params);
+    headers: Ember.computed(function() {
+        return {
+            'Girder-Token': this.get('tokenHandler').getWholeTaleAuthToken()
+            //      'Girder-Token': Ember.get(document.cookie.match(/girderToken\=([^;]*)/), '1'),  };
+        };
+    }),
+
+    // Some Girder APIs need query params in the url to update models...
+    // This functions allows us to pass query params to save() like this:
+    // model.save({adapterOptions:{queryParams:{}}});
+    // where queryParams is a hash object.
+    urlForUpdateRecord(id, modelName, snapshot) {
+        let url = this._super(id, modelName, snapshot);
+        if(snapshot.adapterOptions.queryParams) {
+            let queryParams = snapshot.adapterOptions.queryParams;
+            let keys = Object.keys(queryParams);
+            let q = keys.reduce((_q, key) => {
+                _q.push(key+"="+queryParams[key]);
+                return _q;
+            }, []);
+            return url + "?"+q.join('&');
+        }
+        return url;
     },
-  host: config.apiHost,
-  namespace: config.apiPath,
-  primaryKey: '_id',
-  headers: Ember.computed(function() {
-    return {
-      'Girder-Token': this.get('tokenHandler').getWholeTaleAuthToken()
-//      'Girder-Token': Ember.get(document.cookie.match(/girderToken\=([^;]*)/), '1'),  };
-    };
-  })
+
+    // Ember likes "204 no content" responses when making a DELETE request.
+    // But Girder returns 200, so we have to override the response to prevent
+    // ember from throwing an exception when deleting a model.
+    deleteRecord(store, type, snapshot) {
+        return null;
+    },
+
+    methodForRequest(params) {
+        if (params.requestType === 'createRecord') { return 'PUT'; }
+        return this._super(params);
+    },
 });
 
 //.volatile()
