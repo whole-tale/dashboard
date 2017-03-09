95fa8556c13657f8a097068f7d802668ea6b89ca
Adam Brinckman <abrinckm@gmail.com>
2017-03-08 22:23:25 -0500

    Override the default rest adapter to accept query params. Some girder APIs need this to save the models

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,6 +6,24 @@ import config from '../config/environment';
 export default DS.RESTAdapter.extend({
   tokenHandler: Ember.inject.service("token-handler"),
 
+  // Some Girder APIs need query params in the url to update models...
+  // This functions allows us to pass query params to save() like this:
+  // model.save({adapterOptions:{queryParams:{}}});
+  // where queryParams is a hash object.
+  urlForUpdateRecord(id, modelName, snapshot) {
+    let url = this._super(id, modelName, snapshot);
+    if(snapshot.adapterOptions.queryParams) {
+        let queryParams = snapshot.adapterOptions.queryParams;
+        let keys = Object.keys(queryParams);
+        let q = keys.reduce((_q, key) => {
+            _q.push(key+"="+queryParams[key]);
+            return _q;
+        }, []);
+        return url + "?"+q.join('&');
+    }
+    return url;
+  },
+
   methodForRequest(params) {
 //      console.log("MethodForRequest being called...");
   //    console.log(params);
