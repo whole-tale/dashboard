0b8e72c949ca672ee0aa269773b5f060d127505f
ian-taylor <ian.j.taylor@gmail.com>
2017-03-30 22:32:04 -0400

    rewrite of the explore screen ino tabs for easier navigation.

--- app/controllers/explore.js
+++ app/controllers/explore.js
@@ -2,11 +2,39 @@ import Ember from 'ember';
 import EmberUploader from 'ember-uploader';
 
 export default Ember.Controller.extend({
+  registeredActive: "active",
+  imagesActive: "",
+  talesActive: "",
+  showRegistered: true,
+  showImages: false,
+  showTales: false,
   init() {
 
   },
   actions: {
-    test: function() {
+    registered: function() {
+      this.set("registeredActive", "active");
+      this.set("imagesActive", "");
+      this.set("talesActive", "");
+      this.set("showRegistered", true);
+      this.set("showImages",false);
+      this.set("showTales", false);
+    },
+    images: function() {
+      this.set("registeredActive", "");
+      this.set("imagesActive", "active");
+      this.set("talesActive", "");
+      this.set("showRegistered", false);
+      this.set("showImages",true);
+      this.set("showTales", false);
+    },
+    tales: function() {
+      this.set("registeredActive", "");
+      this.set("imagesActive", "");
+      this.set("talesActive", "active");
+      this.set("showRegistered", false);
+      this.set("showImages",false);
+      this.set("showTales", true);
     },
   }
 });
