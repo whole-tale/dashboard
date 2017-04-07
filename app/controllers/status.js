25215ca8a12d180ba31860a17c8a9e8bfca4ed95
ian-taylor <ian.j.taylor@gmail.com>
2017-04-07 14:38:45 -0400

    launch notebook

--- app/controllers/status.js
+++ app/controllers/status.js
@@ -15,7 +15,7 @@ export default Ember.Controller.extend({
       console.log(item);
       console.log("Tale ID is " + item.get('taleId'));
 
-      item.set("fullUrl", "/" + item.get('url'));
+      item.set("fullUrl", "http://wttmpnb.hub.yt/" + item.get('url'));
 
       controller.get('store').findRecord('tale', item.get('taleId')).
       then(tale => {
