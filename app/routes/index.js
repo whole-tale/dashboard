80357c3f2a6e5f54b4a68b8dc402aea627b28fce
Adam Brinckman <abrinckm@gmail.com>
2017-04-11 11:56:13 -0400

    Fix #18 'New published tales etal. doesn't load from api without page refresh'

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,11 +5,10 @@ import AuthenticateRoute from 'wholetale/routes/authenticate';
 
 export default AuthenticateRoute.extend({
   model() {
-
       return {
-        images: this.get('store').findAll('image'),
-        tales: this.get('store').findAll('tale'),
-        dataRegistered: this.get('store').query('folder', {adapterOptions:{registered: true}})
+        images: this.get('store').findAll('image', {reload: true}),
+        tales: this.get('store').findAll('tale', {reload: true}),
+        dataRegistered: this.get('store').query('folder', {adapterOptions:{registered: true}}, {reload: true})
       };
   }
 });
