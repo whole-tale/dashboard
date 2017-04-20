0d630c00948d610256cf67d709967c5261902edb
Adam Brinckman <abrinckm@gmail.com>
2017-04-20 12:20:36 -0400

    Added test helper 'login-user'

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,28 +1,10 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wholetale/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | index', {
-    before() {
-    }
-});
+moduleForAcceptance('Acceptance | index', {});
 
-test('authenticate user before routing to /index', function(assert) {
-    const server = window.server;
-    
-    server.create('user', {id:1});
-    server.createList('image', 3);
-    server.createList('tale', 3);
-    server.createList('folder', 3);
-
-    let d = new Date();
-    d.setTime(d.getTime() + (180*24*60*60*1000));
-    let expires = d.toUTCString();
-    document.cookie = "girderToken=blah;" +
-        ";path="+
-        ";domain="+
-        ";expires=" + expires;
-
-    visit('/');
+test('should authenticate user before routing to /index', function(assert) {
+    loginUser();
 
     andThen(function() {
         assert.equal(currentURL(), '/');
---
+++ tests/helpers/login-user.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+// Initialize the DB with fake data for the homepage and login the user
+// This helper should typically be invoked before each test that requires
+// an authenticated session.
+export default Ember.Test.registerAsyncHelper('loginUser', function(app) {
+    const server = window.server;
+
+    server.create('user', {id:1});
+    server.createList('image', 3);
+    server.createList('tale', 3);
+    server.createList('folder', 3);
+
+    let d = new Date();
+    d.setTime(d.getTime() + (180*24*60*60*1000));
+    let expires = d.toUTCString();
+    document.cookie = "girderToken=blah;" +
+        ";path="+
+        ";domain="+
+        ";expires=" + expires;
+
+    visit('/');
+    return wait();
+});
