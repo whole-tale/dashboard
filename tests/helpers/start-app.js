60d1bbdaf195d980641eb16e25bd8fd90858642a
Adam Brinckman <abrinckm@gmail.com>
2017-04-20 12:17:16 -0400

    Turn off deprecation warnings during tests... (Too verbose)

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,6 +1,38 @@
 import Ember from 'ember';
+import loginUser from './login-user';
 import Application from '../../app';
 import config from '../../config/environment';
+
+const alreadyShownFactory = () => {
+  let alreadyShown = [];
+  return (msg, test, opt) => {
+    if (test)
+      return false;
+
+    if( alreadyShown.indexOf(msg) === -1 ) {
+      let warning = 'DEPRECATION: ' + msg;
+      if(opt && opt.url) {
+        warning += ' See: ' + opt.url;
+      }
+      console.warn(warning);
+      alreadyShown.push(msg);
+    }
+  };
+};
+Ember.deprecate = alreadyShownFactory();
+Ember.warn = alreadyShownFactory();
+
+//see https://guides.emberjs.com/v2.3.0/configuring-ember/handling-deprecations/
+Ember.Debug.registerDeprecationHandler((() => {
+  let alreadyShown = [];
+  return (message, options, next) => {
+    // if(alreadyShown.indexOf(message) === -1) {
+    if(false) {
+      next(message, options);
+      alreadyShown.push(message);
+    }
+  };
+})());
 
 export default function startApp(attrs) {
   let application;
