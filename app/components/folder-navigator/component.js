49feba1b1cb7ce423c60307e22421155e41c6251
Adam Brinckman <abrinckm@gmail.com>
2017-07-28 10:59:20 -0400

    match proper user folders in My Data view

--- app/components/folder-navigator/component.js
+++ app/components/folder-navigator/component.js
@@ -3,10 +3,14 @@ import layout from './template';
 
 export default Ember.Component.extend({
     folderNavs : Ember.inject.service(),
+    userAuth: Ember.inject.service(),
+
     layout,
     init () {
       this._super(...arguments);
       this.set("navs", this.get('folderNavs').getFolderNavs());
+      this.set("user", this.get('userAuth').getCurrentUser());
+      console.log(this.user);
     },
     actions: {
         navClicked : function(nav) {
