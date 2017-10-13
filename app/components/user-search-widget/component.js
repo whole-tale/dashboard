5f010730250e6d09f44e302799619b0bbcda841d
Sebastian Wyngaard <swyngaar@nd.edu>
2017-10-13 14:37:41 -0400

    Add user search component

--- app/components/user-search-widget/component.js
+++ app/components/user-search-widget/component.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  init() {
+    this._super(...arguments);
+    let self = this;
+    let store = this.get('store');
+
+    Ember.run.scheduleOnce('afterRender', function(){
+      store.query('user', {}).then(_users => {
+        let content = _users.reduce(function(_content, _user) {
+          let obj = { title: _user.get('firstName') + " " + _user.get('lastName'), userObject: _user };
+          _content.push(obj);
+          return _content;
+        }, []);
+        $('.ui.search')
+          .search({
+            source: content,
+            onSelect: self.userSelected.bind(self)
+          });
+      });
+    })
+
+  },
+
+  userSelected(result, response) {
+    this.sendAction('action', result.userObject);
+  },
 });
