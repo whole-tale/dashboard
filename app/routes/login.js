bbe207ac04e920e9865083860404ec9e0609111c
Thomas Thelen <tommythelen@gmail.com>
2018-01-22 13:27:46 -0800

    Redirect the User to Last Page Upon Login
    This change will place the user back at the last page they had visitied when the log in. For example, if the user visits /compose and aren't logged in, upon logging in they will be redirected back to /compose. The particualr applicaiton of this is when a user that isn't logged in tries to open a Data One package and needs to log in.

--- app/routes/login.js
+++ app/routes/login.js
@@ -7,7 +7,11 @@ export default Ember.Route.extend({
     var http = location.protocol;
     var slashes = http.concat("//");
     var ishttp = (location.port === '') || (location.port === 80) || (location.port === 443);
-    var host = slashes.concat(window.location.hostname) + (ishttp? "": ':'+location.port);
+    // Check if the user is coming from the login screen. If they are, we don't want to redirect them back to it.
+    var islogin = (window.location.pathname == '/\login');
+    var host = slashes.concat(window.location.hostname) +
+     (ishttp? "": ':'+location.port) +
+      (islogin? "": encodeURIComponent(window.location.pathname));
     let url = config.apiUrl + '/oauth/provider?redirect=' + host + "%3Ftoken%3D%7BgirderToken%7D";
 
     return Ember.$.getJSON(url);
