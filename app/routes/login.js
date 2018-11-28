7b6dad484c7f8e6a50a16321d90434441ec1f5f7
Mike Lambert <lambert8@illinois.edu>
2018-11-28 16:56:30 -0600

    Preserve return route for non-logged-in users

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,14 +1,23 @@
 import Ember from 'ember';
+import { inject as service } from '@ember/service';
 import config from '../config/environment';
 
 export default Ember.Route.extend({
+  queryParams: {
+    rd: {
+      refreshModel: true
+    }
+  },
+  routerService: service('-routing'),
   model: function(params) {
     // console.log("In login route");
     var http = location.protocol;
     var slashes = http.concat("//");
     var ishttp = (location.port === '') || (location.port === 80) || (location.port === 443);
     var host = slashes.concat(window.location.hostname) + (ishttp? "": ':'+location.port);
-    let url = config.apiUrl + '/oauth/provider?redirect=' + host + "%3Ftoken%3D%7BgirderToken%7D";
+    console.log("Route: ", this.routerService);
+    var pathSuffix = params.rd || "";
+    let url = config.apiUrl + '/oauth/provider?redirect=' + host + pathSuffix + "%3Ftoken%3D%7BgirderToken%7D";
 
     return Ember.$.getJSON(url);
   }
