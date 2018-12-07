53de3f8419651e62d33e7bf26d994426e1b3959e
Damian Perez <cyberdante@gmail.com>
2018-12-07 14:20:18 -0500

    Fix #302. Add tabs component to the Run view

--- app/components/ui/tale-tab-files/component.js
+++ app/components/ui/tale-tab-files/component.js
@@ -1,9 +1,9 @@
 import Component from '@ember/component';
 
-
 export default Component.extend({
-  model: null,
-  init() {
-    this._super(...arguments);
-  },
+    model: null,
+    
+    init() {
+        this._super(...arguments);
+    }
 });
--- app/helpers/truncate-name.js
+++ app/helpers/truncate-name.js
@@ -1,12 +1,18 @@
-import Ember from 'ember';
-import moment from 'moment';
+import { helper } from '@ember/component/helper';
 
-
-export default Ember.Helper.helper(function(params) {
-  let [name] = params;
-  if (name == null) return "";
-  if (name.length > 35)
-    name= name.substring(0,35) + "...";
-
-  return name;
-});
+export default helper(function truncateText(params, hash) {
+    const [ value ] = params;
+    let { limit } = hash;
+    limit = limit || 35;
+    let text = '';
+  
+    if (value != null && value.length > 0) {
+      text = value.substr(0, limit);
+  
+      if (value.length > limit) {
+        text += '...';
+      }
+    }
+  
+    return text;
+  });
