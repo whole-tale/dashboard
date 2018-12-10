f73c565dd6a5877d73c2dbbaf41d9457730976fa
Damian Perez <cyberdante@gmail.com>
2018-12-10 11:39:31 -0500

    Add tabbed structure for running tales view

--- app/components/ui/tale-tab-files/component.js
+++ app/components/ui/tale-tab-files/component.js
@@ -2,8 +2,24 @@ import Component from '@ember/component';
 
 
 export default Component.extend({
-  model: null,
-  init() {
-    this._super(...arguments);
-  },
+    model: null,
+    classNames: ['tale-tab-files'],
+    
+    init() {
+        this._super(...arguments);
+    },
+    actions: {
+        itemClicked() {
+            this.get('itemClicked')();
+        },
+        refresh() {
+            this.get('refresh')();
+        },
+        breadcrumbClicked() {
+            this.get('breadcrumbClicked')();
+        },
+        navClicked() {
+            this.get('navClicked')();
+        }
+    }
 });
