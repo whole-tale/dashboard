50ddbc6a33348696d8062e6fa5b25f8d7a546af0
Damian Perez <cyberdante@gmail.com>
2018-12-11 11:44:47 -0500

    Match files tab comp

--- app/services/folder-navs.js
+++ app/services/folder-navs.js
@@ -1,80 +1,84 @@
 import Service from '@ember/service';
 import {
-  inject as service
+    inject as service
 } from '@ember/service';
 
 export default Service.extend({
-  userAuth: service(),
-  internalState: service(),
+    userAuth: service(),
+    internalState: service(),
 
-  getFolderNavs: function () {
-    const thisUserID = this.get('userAuth').getCurrentUserID();
+    getFolderNavs: function () {
+        const thisUserID = this.get('userAuth').getCurrentUserID();
 
-    return [{
-        name: "Home",
-        command: "home",
-        parentId: thisUserID,
-        parentType: "user",
-        icon: "home",
-        isFolder: true,
-        instructions: 'Synced to local machine',
-        allowUpload: true,
-        allowRegister: false,
-        disallowImport: false
-      },
-      {
-        name: "Data",
-        command: "user_data",
-        parentId: thisUserID,
-        parentType: "user",
-        icon: "linkify",
-        isFolder: true,
-        instructions: 'Linked from external sources',
-        allowUpload: false,
-        allowRegister: true,
-        disallowImport: true
-      },
-      /*
-      {
-        name : "Results",
-        command : "workspace",
-        parentId : thisUserID,          //TODO: Find out what the parent is
-        parentType : "user",
-        icon : "folder",
-        isFolder: true, //TODO: Find out whether this is an actual folder
-        instructions: 'Output from Tales',
-        allowUpload: true
-      },
-      {
-        name : "Recent",
-        command : "recent",
-        parentId : null,
-        parentType : null,
-        icon : "calendar",
-        isFolder: false,
-        instructions: null
-      }
-      */
-    ];
-  },
-  getFolderNavFor: function (navCommand) {
-    let  navs = this.getFolderNavs();
-    //console.log(navs);
+        return [{
+            name: "Home",
+            command: "home",
+            parentId: thisUserID,
+            parentType: "user",
+            icon: "home",
+            isFolder: true,
+            instructions: 'Synced to local machine',
+            allowUpload: true,
+            allowRegister: false,
+            disallowImport: false,
+            description: 'Global directory accessible across all Tales; can be synced to local machine'
+        },
+        {
+            name: "Data",
+            command: "user_data",
+            parentId: thisUserID,
+            parentType: "user",
+            icon: "linkify",
+            isFolder: true,
+            instructions: 'Linked from external sources',
+            allowUpload: false,
+            allowRegister: true,
+            disallowImport: true,
+            description: 'Linked, external data for use as Tale input (read-only)'
+        },
+        {
+            name: "Tale Workspace",
+            command: "workspace",
+            parentId: thisUserID,
+            parentType: "user",
+            icon: "folder",
+            isFolder: true,
+            instructions: 'Output from Tales',
+            allowUpload: true,
+            // allowRegister: false,
+            disallowImport: false,
+            description: 'All files associated with this Tale, except external data and global files in Home'
+        }];
+        /*,
+        {
+          name : "Recent",
+          command : "recent",
+          parentId : null,
+          parentType : null,
+          icon : "calendar",
+          isFolder: false,
+          instructions: null
+        }
+        */
+    },
+    getFolderNavFor: function (navCommand) {
+        let navs = this.getFolderNavs();
+        //console.log(navs);
 
-    for (let  i = 0; i < navs.length; ++i) {
-      //console.log(navs[i]);
-      if (navs[i].command === navCommand) return navs[i];
+        for (let i = 0; i < navs.length; ++i) {
+            //console.log(navs[i]);
+            if (navs[i].command === navCommand) return navs[i];
+        }
+        return null;
+    },
+    getCurrentFolderNavAndSetOn: function (obj) {
+        let navCommand = this.get('internalState').getCurrentNavCommand() || 'home';
+        // console.log("Nav Command = " + navCommand);
+        let currentNav = this.getFolderNavFor(navCommand);
+        obj.set("currentNavCommand", navCommand);
+        obj.set("currentNavTitle", currentNav.name);
+        obj.set("currentNav", currentNav);
+        return currentNav;
     }
-    return null;
-  },
-  getCurrentFolderNavAndSetOn: function (obj) {
-    let  navCommand = this.get('internalState').getCurrentNavCommand() || 'home';
-    // console.log("Nav Command = " + navCommand);
-    let  currentNav = this.getFolderNavFor(navCommand);
-    obj.set("currentNavCommand", navCommand);
-    obj.set("currentNavTitle", currentNav.name);
-    obj.set("currentNav", currentNav);
-    return currentNav;
-  }
 
 });
