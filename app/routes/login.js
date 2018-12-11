1ef8387ecc62c234a73deca1c991377c1d5e4b69
Damian Perez <cyberdante@gmail.com>
2018-12-11 12:19:42 -0500

    Remove linting errors
    Also adds missing semicolons

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,34 +1,35 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import config from '../config/environment';
+import $ from 'jquery';
 
-export default Ember.Route.extend({
+export default Route.extend({
   queryParams: {
     rd: {
       refreshModel: true
     }
   },
   routerService: service('-routing'),
-  model: function(params) {
+  model(params) {
     // console.log("In login route");
-    var http = location.protocol;
-    var slashes = http.concat("//");
-    var ishttp = (location.port === '') || (location.port === 80) || (location.port === 443);
-    var host = slashes.concat(window.location.hostname) + (ishttp? "": ':'+location.port);
-    var pathSuffix = decodeURIComponent(params.rd) || "";
-    var redirectUrl = host;
+    let http = location.protocol;
+    let slashes = http.concat('//');
+    let ishttp = (location.port === '') || (location.port === 80) || (location.port === 443);
+    let host = slashes.concat(window.location.hostname) + (ishttp ? '' : ':' + location.port);
+    let pathSuffix = decodeURIComponent(params.rd) || '';
+    let redirectUrl = host;
     // Append return route if one exists, ignore login route
     if (pathSuffix && pathSuffix.indexOf('/login') === -1) {
-      redirectUrl += pathSuffix
+      redirectUrl += pathSuffix;
     }
     // Append to query string if one exists, otherwise add one
-    if (redirectUrl.indexOf("?") !== -1) {
-      redirectUrl += "&token={girderToken}"
+    if (redirectUrl.indexOf('?') !== -1) {
+      redirectUrl += '&token={girderToken}';
     } else {
-      redirectUrl += "?token={girderToken}"
+      redirectUrl += '?token={girderToken}';
     }
     let url = config.apiUrl + '/oauth/provider?redirect=' + encodeURIComponent(redirectUrl);
 
-    return Ember.$.getJSON(url);
+    return $.getJSON(url);
   }
 });
