2e42ef58a41b878256b75fefdade8443333047af
Damian Perez <cyberdante@gmail.com>
2018-12-14 09:49:54 -0500

    Better align directory-browser content. Remove description

--- app/helpers/bytes-to-readable.js
+++ app/helpers/bytes-to-readable.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function bytesToReadable(size/*, hash*/) {
   var i = Math.floor( Math.log(size) / Math.log(1024) );
-  var txt = ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+  var txt = ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
   if (txt == "NaN undefined")
     return "0";
   else
