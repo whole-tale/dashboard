5eeafb45ddc3a355f042436fc2fe2cac996d1a60
Thomas Thelen <tommythelen@gmail.com>
2019-02-22 14:20:48 -0800

    Add the published URL to the metadata view

--- app/components/ui/tale-tab-metadata/component.js
+++ app/components/ui/tale-tab-metadata/component.js
@@ -19,6 +19,7 @@ export default Component.extend({
   environments: [],
   licenses: [],
   model: null,
+  publishedURL: 'This Tale is un-published',
   init() {
     this._super(...arguments);
     
