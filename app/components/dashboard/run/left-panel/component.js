544ff690451d075f93f4766fa7779c7a90d951c9
Thomas Thelen <tommythelen@gmail.com>
2019-03-06 10:22:54 -0800

    Add a link to export a Tale to the Run page

--- app/components/dashboard/run/left-panel/component.js
+++ app/components/dashboard/run/left-panel/component.js
@@ -8,7 +8,6 @@ import { A } from '@ember/array';
 import { not } from '@ember/object/computed';
 import $ from 'jquery';
 import layout from './template';
-// import hasEmberVersion from 'ember-test-helpers/has-ember-version';
 
 const O = Object.create.bind(Object);
 
@@ -18,12 +17,14 @@ export default Component.extend(FullScreenMixin, {
     router: service('-routing'),
     internalState: service(),
     apiCall: service('api-call'),
+    tokenHandler: service('token-handler'),
     loadError: false,
     model: null,
     wholeTaleHost: config.wholeTaleHost,
     hasSelectedTaleInstance: false,
     displayTaleInstanceMenu: false,
     workspaceRootId: undefined,
+    showDimmer: false,
 
     session: O({dataSet:A()}),
 
@@ -165,5 +166,63 @@ export default Component.extend(FullScreenMixin, {
         denyDelete() {
             return true;
         },
+        
+        exportTale(id) {
+          let self = this;
+          // Asks the girder endpoint for a zipped Tale
+          // This code was adapted from
+          // https://stackoverflow.com/questions/16086162/handle-file-download-from-ajax-post
+          const token = self.get('tokenHandler').getWholeTaleAuthToken();
+          let url = `${config.apiUrl}/tale/${id}/export`;
+          var xhr = new XMLHttpRequest();
+          xhr.open('POST', url, true);
+          xhr.responseType = 'arraybuffer';
+          xhr.setRequestHeader("Girder-Token", token);
+          self.set('showDimmer', true);
+          xhr.onload = function () {
+              if (this.status === 200) {
+                self.set('showDimmer', false);
+                  var filename = "";
+                  var disposition = xhr.getResponseHeader('Content-Disposition');
+                  if (disposition && disposition.indexOf('attachment') !== -1) {
+                      var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                      var matches = filenameRegex.exec(disposition);
+                      if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                  }
+                  var type = xhr.getResponseHeader('Content-Type');
+    
+                  var blob = typeof File === 'function'
+                      ? new File([this.response], filename, { type: type })
+                      : new Blob([this.response], { type: type });
+                  if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                      // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                      window.navigator.msSaveBlob(blob, filename);
+                  } else {
+                      var URL = window.URL || window.webkitURL;
+                      var downloadUrl = URL.createObjectURL(blob);
+    
+                      if (filename) {
+                          // use HTML5 a[download] attribute to specify filename
+                          var a = document.createElement("a");
+                          // safari doesn't support this yet
+                          if (typeof a.download === 'undefined') {
+                              window.location = downloadUrl;
+                          } else {
+                              a.href = downloadUrl;
+                              a.download = filename;
+                              document.body.appendChild(a);
+                              a.click();
+                          }
+                      } else {
+                          window.location = downloadUrl;
+                      }
+    
+                      setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                  }
+              }
+          };
+          xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+          xhr.send();
+        }
     }
 });
