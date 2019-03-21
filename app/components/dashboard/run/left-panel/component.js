ffa7fea4d38b59bf27726ed80295661cf61da6a3
Thomas Thelen <tommythelen@gmail.com>
2019-03-21 14:48:47 -0700

    Turn the Export license item into a dropdown so that a user can select bagit and zip

--- app/components/dashboard/run/left-panel/component.js
+++ app/components/dashboard/run/left-panel/component.js
@@ -167,62 +167,22 @@ export default Component.extend(FullScreenMixin, {
             return true;
         },
         
-        exportTale(id) {
-          let self = this;
-          // Asks the girder endpoint for a zipped Tale
-          // This code was adapted from
-          // https://stackoverflow.com/questions/16086162/handle-file-download-from-ajax-post
-          const token = self.get('tokenHandler').getWholeTaleAuthToken();
+        exportZip(id) {
+          const token = this.get('tokenHandler').getWholeTaleAuthToken();
           let url = `${config.apiUrl}/tale/${id}/export`;
-          var xhr = new XMLHttpRequest();
-          xhr.open('GET', url, true);
-          xhr.responseType = 'arraybuffer';
-          xhr.setRequestHeader("Girder-Token", token);
-          self.set('showDimmer', true);
-          xhr.onload = function () {
-              if (this.status === 200) {
-                self.set('showDimmer', false);
-                  var filename = "";
-                  var disposition = xhr.getResponseHeader('Content-Disposition');
-                  if (disposition && disposition.indexOf('attachment') !== -1) {
-                      var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-                      var matches = filenameRegex.exec(disposition);
-                      if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
-                  }
-                  var type = xhr.getResponseHeader('Content-Type');
-    
-                  var blob = typeof File === 'function'
-                      ? new File([this.response], filename, { type: type })
-                      : new Blob([this.response], { type: type });
-                  if (typeof window.navigator.msSaveBlob !== 'undefined') {
-                      // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-                      window.navigator.msSaveBlob(blob, filename);
-                  } else {
-                      var URL = window.URL || window.webkitURL;
-                      var downloadUrl = URL.createObjectURL(blob);
-    
-                      if (filename) {
-                          // use HTML5 a[download] attribute to specify filename
-                          var a = document.createElement("a");
-                          // safari doesn't support this yet
-                          if (typeof a.download === 'undefined') {
-                              window.location = downloadUrl;
-                          } else {
-                              a.href = downloadUrl;
-                              a.download = filename;
-                              document.body.appendChild(a);
-                              a.click();
-                          }
-                      } else {
-                          window.location = downloadUrl;
-                      }
-    
-                      setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
-                  }
-              }
-          };
-          xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-          xhr.send();
-        }
+          window.open(url + '?token=' + token + '&taleFormat=native', '_newtab');
+        },
+
+        exportBag(id) {
+          const token = this.get('tokenHandler').getWholeTaleAuthToken();
+          let url = `${config.apiUrl}/tale/${id}/export`;
+          window.open(url + '?token=' + token + '&taleFormat=bagit', '_newtab');
+        },
+
+        actionMenuClicked() {
+          // We need to initialize the export dropdown each time the 
+          // tale action menu is opened
+          $('.ui.left.pointing.dropdown.link.item').dropdown();
+        },
     }
 });
