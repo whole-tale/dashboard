3f1df2cc1e114a397012843e705df1d7603befb1
Thomas Thelen <tommythelen@gmail.com>
2019-03-24 18:11:06 -0700

    Add additional fields in the Tale metadata tab for adding and removing Tale authors

--- app/components/ui/tale-tab-metadata/component.js
+++ app/components/ui/tale-tab-metadata/component.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-
+import Object, { computed } from '@ember/object';
+import { A } from '@ember/array';
 import $ from 'jquery';
 
 import config from '../../../config/environment';
@@ -20,9 +20,15 @@ export default Component.extend({
   licenses: [],
   model: null,
   publishedURL: 'This Tale has not been published',
+  // Array of author dicts that the template uses to render rows
+  taleAuthors: A(),
+  // An error message that can get set, displayed in the error modal
+  errorMessage: String(),
+  model: null,
+  cannotEditTale: computed.not('canEditTale').readOnly(),
   init() {
     this._super(...arguments);
-    
+
     // Fetch images for user to select an environment
     const component = this;
     $.getJSON(this.get('apiHost') + '/api/v1/image/').then(function(images) {
@@ -42,7 +48,16 @@ export default Component.extend({
       this.set('publishedURL', tale.publishInfo[tale.publishInfo.length - 1].uri );
     }
   },
-  
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    // Re-render the rows by discarding the previous state and
+    // re-adding the authors
+    this.taleAuthors.clear();
+    this.addTaleAuthors();
+  },
+
   didRender() {
     // Enable the environment dropdown functionality
     $('.ui.dropdown').dropdown();
@@ -70,18 +85,110 @@ export default Component.extend({
   canEditTale: computed('model.tale._accessLevel', function () {
     return this.get('model') && this.get('model').get('tale') && this.get('model').get('tale').get('_accessLevel') >= taleStatus.WRITE;
   }).readOnly(),
-  cannotEditTale: computed.not('canEditTale').readOnly(),
+      
+   /* 
+    Generates a uui that can assigned to a tale author. Note that this change 
+    allows identification about which author row is being interacted with. This
+    change does not persist to the backend.
+   */
+  ID() {
+    let array = new Uint32Array(8)
+    window.crypto.getRandomValues(array)
+    let str = ''
+    for (let i = 0; i < array.length; i++) {
+      str += (i < 2 || i > 5 ? '' : '-') + array[i].toString(16).slice(-4)
+    }
+    return str
+  },
+
+  /* 
+    Adds the Tale's saved authors to the component property
+    that the handlebars template uses to create rows (this.taleAuthors)
+  */
+ addTaleAuthors() {
+  let authors = this.get('model').get('tale').authors;
   
+  for (let i = 0; i < authors.length; i++) {
+    let newAuthor = {
+      'id': this.ID(),
+      'firstName': authors[i].firstName,
+      'lastName': authors[i].lastName,
+      'orcid': authors[i].orcid
+  }
+    this.taleAuthors.pushObject(newAuthor);
+  }
+},
+
+  /* 
+    Iterates over the array of saved&unsaved authors and makes sure that
+      1. There is at least one author
+      2. All of the authors have a first & last name and and an ORCID
+  */
+  validateAuthors () {
+    if (this.taleAuthors.length < 1) {
+      this.set('errorMessage', 'Your Tale must have at least one author; please add one to the Tale.');
+      return false
+    }
+    let res = true;
+    this.taleAuthors.forEach((author) => {
+      if (author.lastName.length < 1) {
+        this.set('errorMessage', "There is a Tale author that's missing a last name.");
+        res = false
+      }
+      if (author.firstName.length < 1 ) {
+        this.set('errorMessage', "There is a Tale author that's missing a first name.");
+        res = false;
+      }
+      if (author.orcid.length < 1) {
+        this.set('errorMessage', "There is a Tale author that's missing an ORCID.");
+        res = false;
+      }
+    });
+    return res;
+  },
+  
+  /* 
+    There is a discrepency between the Tale 'authors' object and the ones used in the
+    component. The ID field needs to be removed before adding the structure to the model.
+    This method formats the component object and updates the Tale model with it.
+  */
+  addAuthorsTaleModel() {
+    let taleAuthors = []
+    this.taleAuthors.forEach((author) => {
+      taleAuthors.push(
+        {
+          'firstName': author.firstName,
+          'lastName': author.lastName,
+          'orcid': author.orcid
+        })
+      })
+    this.model.tale.set('authors', taleAuthors);
+  },
   actions: {
+
+    /* 
+      Takes the fields in the UI and updates the Tale model with the new
+      values.
+    */
     updateTale() {
       const tale = this.get('model').get('tale');
       const onFail = (e) => {
-        alert('Error saving tale (' + tale['id'] + '):', e);
+        this.set('errorMessage', ('Error saving tale (' + tale['id'] + '):', e));
+        this.send('openErrorModal');
       };
-      
-      tale.save().catch(onFail);
+
+      // Only update the Tale if the authors are valid
+      if (this.validateAuthors()) {
+        // Preform any author formatting
+        this.addAuthorsTaleModel()
+        // Request that the Tale model be updated
+        tale.save().catch(onFail);
+      }
+      else {
+        this.send('openErrorModal');
+      }
     },
-      
+
     setTaleEnvironment: function(selected) {
       const tale = this.get('model').get('tale');
       tale.set('imageId', selected);
@@ -90,6 +197,48 @@ export default Component.extend({
     setTaleLicense: function(selected) {
       const tale = this.get('model').get('tale');
       tale.set('licenseSPDX', selected);
+    },
+    
+    /* 
+    Adds a new, blank user to the component authors object, which
+    then gets added as a new row in the UI.
+    */
+    addNewAuthor() {
+      let newAuthor = {
+        'id': this.ID(),
+        'firstName': '',
+        'lastName': '',
+        'orcid':'',
+      }
+      this.taleAuthors.pushObject(newAuthor)
+    },
+
+    /* 
+    Removes an author from the component, and is consequentially removed
+    in the UI.
+    */
+    removeAuthor(id) {
+      let currentAuthors = this.get('taleAuthors');
+      let deletedAuthor = currentAuthors.find(o => o.id === id);
+      if(deletedAuthor){
+        let authorLocation = currentAuthors.indexOf(deletedAuthor);
+        if (authorLocation > -1) {
+          this.taleAuthors.removeAt(authorLocation);
+        }
+      }
+    },
+
+    /* 
+      Open the modal that the user sees when an error occurred or when validation failed.
+    */
+    openErrorModal() {
+      let selector = '.ui.metadata-error.modal';
+      $(selector).modal('setting', 'closable', true);
+      $(selector).modal('show');
+    },
+    
+    closeErrorModal() {
+      return false;
     },
   }
 });
