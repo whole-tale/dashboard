f5a5e7b2b04e459b475ccfb5bc204b40a9b39408
Mike Lambert <lambert8@illinois.edu>
2019-05-02 17:50:16 +0000

    Checkpoint: added tab selector to Browse view, added pretty intro

--- app/components/dashboard/browse/left-panel/component.js
+++ app/components/dashboard/browse/left-panel/component.js
@@ -1,9 +1,24 @@
-import Ember from 'ember';
+import Component from '@ember/component';
 import FullScreenMixin from 'ember-cli-full-screen/mixins/full-screen';
+import $ from 'jquery';
 
-export default Ember.Component.extend(FullScreenMixin, {
 
+export default Component.extend(FullScreenMixin, {
+
+  currentTab: 'tales',
+  
   actions: {
+    selectTab(tabName) {
+      this.set('currentTab', tabName);
+    },
+    showIntroduction() {
+      $('#introduction-short').transition('slide up');
+      $('#introduction-long').transition('slide down');
+    },
+    hideIntroduction() {
+      $('#introduction-short').transition('slide up');
+      $('#introduction-long').transition('slide down');
+    },
     gotoPublish : function(name) {
       scroll(0,0);
       this.sendAction("gotoPublish", name);
