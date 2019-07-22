a0490e9569bbeacfb64927e6e873c03af04f2e29
Thomas Thelen <tommythelen@gmail.com>
2019-07-22 16:30:42 -0700

    Remove unused import

--- app/components/ui/create-tale-modal/component.js
+++ app/components/ui/create-tale-modal/component.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import {inject as service} from '@ember/service';
-import {later} from '@ember/runloop';
 import $ from 'jquery';
 
 export default Component.extend({
