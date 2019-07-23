3a5ec5f0d3a56cfc33c766ef252e975382a68cb0
Mike Lambert <lambert8@illinois.edu>
2019-07-23 16:06:45 +0000

    Navigate to Run > Metadata when clicking View from Browse view (also for defunct list view case)

--- app/components/ui/tale-tabs-selector/component.js
+++ app/components/ui/tale-tabs-selector/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { observer } from '@ember/object';
+import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import layout from './template';
 // import hasEmberVersion from 'ember-test-helpers/has-ember-version';
@@ -8,10 +9,32 @@ export default Component.extend({
     layout,
     classNames: ['tale-tabs-selector'],
     internalState: service(),
+    routing: service('-routing'),
+    router: service(),
+    params: alias('routing.router.currentState.routerJsState.fullQueryParams'),
     
     activeTabInteract: true,
     activeTabFiles: false,
     activeTabMetadata: false,
+    
+    init() {
+        this._super(...arguments);
+        
+        // If a "tab" querystring param is specified, consume it
+        if (this.params['tab']) {
+            let queryTab = this.params['tab'];
+            if (queryTab === 'interact') {
+                this.actions.activateInteract.call(this);
+            } else if (queryTab === 'files') {
+                this.actions.activateFiles.call(this);
+            } else if (queryTab === 'metadata') {
+                this.actions.activateMetadata.call(this);
+            }
+            
+            // Clear the querystring parameter after it is consumed
+            this.router.transitionTo({ queryParams: { tab: null }});
+        }
+    },
     
     launchErrorEncountered: observer('taleLaunchError', function() {
         console.log(`taleLaunchError changed: ${this.taleLaunchError}`);
