f001b727661c7e3578ef4177e337de520e2b4882
Mike Lambert <lambert8@illinois.edu>
2019-09-23 16:19:53 +0000

    Fixed Description/GenIllus/License/Environment to be readonly when appropriate

--- app/components/ui/markdown-editor/component.js
+++ app/components/ui/markdown-editor/component.js
@@ -1,10 +1,12 @@
 // app/pods/components/markdown-component/component.js
-import Ember from 'ember';
+import Component from '@ember/component';
+import { later } from '@ember/runloop';
 import layout from './template';
 
-export default Ember.Component.extend({
+export default Component.extend({
     layout,
 
+    readonly: false,
     markdownValue: "",
 
     didInsertElement() {
@@ -12,9 +14,15 @@ export default Ember.Component.extend({
           .tab()
         ;
 
+        if (this.get('readonly')) {
+            later(function() {
+                $('.ui.menu').find('.item').tab('change tab', 'preview');
+            },500);
+        }
+
         if(!this.markdownValue) {
             let component = this;
-            Ember.run.later(function() {
+            later(function() {
                 component.set('markdownValue', "#### Markdown Editor");
             },1);
         }
