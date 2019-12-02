f9ee7c210a964f1883c7615c3021404f45a0705a
Mike Lambert <lambert8@illinois.edu>
2019-12-02 21:15:56 +0000

    Allow user to configure DataONE account from User Settings view

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -3,6 +3,7 @@ import EmberObject, { computed } from '@ember/object';
 import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 import { sort } from '@ember/object/computed';
+import config from '../config/environment';
 
 import $ from 'jquery';
 
@@ -26,28 +27,21 @@ export default Controller.extend({
     user: O({}),
     tokens: O({}),
     
-    showConnectExtAccountModal: false,
-    showRevokeExtAccountModal: false,
-    
-    providerTokens: computed('providers', 'tokens', function () {
-      const component = this;
-      const tokens = component.get('tokens');
-      const provider = component.get('tokens');
-      return tokens[provider];
-    }).readOnly(),
-    
     init() {
       this._super(...arguments);
+      this.refreshProviders();
+    },
+    
+    refreshProviders() {
       const component = this;
       
       // Fetch external account providers
       const adapterOptions = { queryParams: { redirect: 'https://dashboard.local.wholetale.org/settings' } };
       component.store.query('account', { adapterOptions }).then(providers => {
         // Of course Ember has their own array implementation... -_-*
-        component.set('providers', providers);
+        component.set('providers', A(providers));
+        component.refreshUserTokens();
       }, err => console.error("Failed to fetch external account providers:", err));
-      
-      this.refreshUserTokens();
     },
     
     refreshUserTokens() {
@@ -56,6 +50,28 @@ export default Controller.extend({
       // Fetch user's configured external tokens
       component.get('userAuth').getCurrentUserFromServer().then(user => {
         component.set('user', user);
+        
+        component.get('providers').forEach(provider => {
+          if (provider.type === 'dataone' && provider.state === 'preauthorized') {
+            let xmlHttp = new XMLHttpRequest();
+        
+            xmlHttp.open("GET", provider.url, false);
+            // Set the response content type
+            xmlHttp.setRequestHeader("Content-Type", "text/xml");
+            // Let XMLHttpRequest know to use cookies
+            xmlHttp.withCredentials = true;
+            xmlHttp.send(null);
+            
+            const data =  xmlHttp.responseText;
+            if (data) {
+              const token = user.otherTokens.find(t => t.provider === provider.name);
+              component.apiCall.authExtToken(provider.name, token.resource_server, data, 'dataone').then(resp => {
+                // Hard reload the page
+                window.location.reload(true);
+              }, err => console.error("Failed to add DataONE API token:", err));
+            }
+          }
+        });
       });
     },
     
@@ -73,9 +89,7 @@ export default Controller.extend({
         component.set('selectedProvider', provider);
         component.apiCall.getExtAccountTargets(provider.name).then(targets => {
           component.get('selectedProvider').set('targets', targets);
-          // TODO: Switch on provider.type to provide a different modal
-          
-          // TODO: pop up a modal for choosing resource_server and entering a new API key
+          // Pop up a modal for choosing resource_server and entering a new API key
           $('#connect-apikey-modal').modal('show');
           $('#newResourceServerDropdown').dropdown();
         }, err => console.error("Failed to fetch provider targets:", err));
